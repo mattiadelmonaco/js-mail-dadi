3d00ae9c8b27dd8fd6121b284e8979780d21c983
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-03 17:30:26 +0100

    feat: add steps

--- Dice game/script.js
+++ Dice game/script.js
@@ -5,3 +5,12 @@
 
 // STEPS:
 
+// 1. Genero un numero randomico da 1 a 6 per il giocatore
+// 2. Genero un numero randomico da 1 a 6 per il computer 
+// 3. SE il numero del giocatore è maggiore rispetto il numero del computer 
+//    comparirà un alert che gli dice che ha vinto e i numeri che sono stati confrontati
+// 4. ALTRIMENTI comparirà un alert che dice che ha perso e che ha vinto il computer e i numeri che sono stati confrontati
+
+// CODE: 
+
+// 1. 
