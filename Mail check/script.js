e5c69a2303818ea3231448b625b819d997aaa263
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-03 16:24:02 +0100

    init project

---
+++ Mail check/script.js
@@ -0,0 +1,15 @@
+// PROBLEM:
+
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Non è necessario provvedere alla validazione delle email.
+// NON POTETE UTILIZZARE NESSUN METODO COME:
+// - includes
+// - indexOf
+// - find
+// - search
+// - ecc...
+
+// STEPS:
+
