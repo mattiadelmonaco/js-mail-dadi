2431360924869a803fb5940a8505a0fda809582d
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-03 16:34:01 +0100

    feat: add steps

--- Mail check/script.js
+++ Mail check/script.js
@@ -13,3 +13,10 @@
 
 // STEPS:
 
+// 1. Creo un array con un elenco di email 
+// 2. Inserisco un prompt per chiedere all'utente di inserire la sua mail
+// 3. Attraverso l'array (con un ciclo for) per far leggere al sistema la lista delle mail accettate
+// 4. Creo una variabile booleana per confermare o meno la presenza della mail
+// 5. SE la variabile booleana è TRUE (quindi mail presente in lista) comparirà un alert con scritto l'esito positivo
+// 6. ALTRIMENTI (variabile booleana FALSE) comparirà un alert con scritto l'esito negativo 
+
