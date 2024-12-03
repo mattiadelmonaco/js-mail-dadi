57336f77a0f0716508f697f69e1bde13bd4d832a
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-03 17:51:59 +0100

    fix: edit steps, add same number

--- Dice game/script.js
+++ Dice game/script.js
@@ -9,7 +9,9 @@
 // 2. Creo una variabile con un numero randomico da 1 a 6 per il computer 
 // 3. SE il numero del giocatore è maggiore rispetto il numero del computer 
 //    comparirà un alert che gli dice che ha vinto e i numeri che sono stati confrontati
-// 4. ALTRIMENTI comparirà un alert che dice che ha perso e che ha vinto il computer e i numeri che sono stati confrontati
+// 4. ALTRIMENTI SE il numero del computer è maggiore rispetto il numero del giocatore
+//    comparirà un alert che dice che ha perso e che ha vinto il computer e i numeri che sono stati confrontati
+// 5. ALTRIMENTI i numeri sono uguali e comprarirà un alert che avviserà del pareggio
 
 // CODE: 
 
